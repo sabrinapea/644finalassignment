e355bea9b1ac2f4618fa15ecce8d7b08ea7076bd
Sabrina <sabrinapea@yahoo.com>
2021-06-02 22:56:38 -0700

    running

--- js/running_total.js
+++ js/running_total.js
@@ -1,5 +1,32 @@
-$('pizzaform').addEventListener('click', (e) => {
-    e.preventDefault();
+// $('pizzaform').addEventListener('click', (e) => {
+//     e.preventDefault();
 
-    let handdough = `Dough selected: ${$('handtossed').value}`;
+//     let handdough = `Dough selected: ${$('handtossed').value}`;
+// }
+
+// Add clicked items to have running total
+function calculate_total() {
+    let elem = document.getElementsById('selectsize');
+    let total = 0;
+
+    for (let i = 0; i < elem.length; i++) {
+        total += parseInt(elem[i].value);
+    }
+    document.getElementById('').value = total;
+    document.write('test');
 }
+calculate_total();
+
+// Get DOM elements
+
+// build the table when the id is clicked
+
+// add options
+
+// prevent submission
+
+// get values from the clicked ids
+
+// build the grid or table
+
+
