01411e330c681c659a32b226e4fa91767c52fcd5
Sabrina <sabrinapea@yahoo.com>
2021-06-07 22:39:27 -0700

    hidden changes

--- js/build_pizza.js
+++ js/build_pizza.js
@@ -16,22 +16,54 @@ const $ = (id) => {
 //             return false;
 //         }
 //     }
-let userPizza = document.querySelector('#buildingpizza');
+let userCheese = document.querySelector('#selectcheese');
 let userDough = document.querySelector('#selectdough');
 let userSize = document.querySelector('#selectsize');
 let userSauce = document.querySelector('#selectsauce');
 let userToppings = document.querySelector('#selecttoppings');
+let userButton = document.querySelector('#buttons');
 let total = 0;
 
 // validate dough and size are clicked before proceeding
-userPizza.addEventListener('click', (e) => {
-    console.log('hello');
-    if (document.getElementById('selectdough').addEventListener('click', () => {
-        if (document.getElementById('size').addEventListener('click', () => {
-            alert('you may proceed');
-        }));
-    }));
-});
+let validateDoughAndSize = function (e) {
+    // validating dough
+    let selectDoughIsValid = false;
+    if (document.getElementById('handtossed').checked ||
+        document.getElementById('thincrust').checked ||
+        document.getElementById('nystyle').checked ||
+        document.getElementById('gluten').checked) {
+        selectDoughIsValid = true;
+        // alert('you are valid');
+    }
+    let selectSizeIsValid = false;
+    if (document.getElementById('size').options[document.getElementById('size').selectedIndex].value != "notvalid") {
+        selectSizeIsValid = true;      
+    }
+    if (selectDoughIsValid && selectSizeIsValid) {
+        userCheese.style.visibility="visible";
+        userSauce.style.visibility="visible";
+        userToppings.style.visibility="visible";
+        userButton.style.visibility="visible";
+    }
+
+    
+    
+    // console.log(document.getElementById('size').options[document.getElementById('size').selectedIndex].value);
+
+    
+}
+userDough.addEventListener('click', validateDoughAndSize);
+userSize.addEventListener('click', validateDoughAndSize);
+
+// userPizza.addEventListener('click', (e) => {
+//     console.log('pie');
+//     if (document.getElementById('selectdough').addEventListener('click', () => {
+//         console.log('hello');
+//         if (document.getElementById('selectsize').addEventListener('click', () => {
+//             alert('you may proceed');
+//         }));
+//     }));
+// });
     
 //     // if(e.target.getElementsByClassName.contains('dough')) {
 //     //     if(e.target.getElementById.contains('size')) {
@@ -52,30 +84,30 @@ function buildPizzaSizeDropDown(e) {
     switch (e.target.id) {
         case "handtossed":
             document.getElementById('size').innerHTML = `
-            <option selected>Your HandTossed Pizza Size Options</option>
-            <option value="1">Small ($9.99)</option>
-            <option value="2">Medium ($12.99)</option>
-            <option value="3">Large ($14.99)</option>
+            <option value="notvalid" selected>Your HandTossed Pizza Size Options</option>
+            <option value="9.99">Small ($9.99)</option>
+            <option value="12.99">Medium ($12.99)</option>
+            <option value="14.99">Large ($14.99)</option>
             `;
             break;
         case "thincrust":
             document.getElementById('size').innerHTML = `
-            <option selected>Your Thincrust Pizza Size Options</option>
-            <option value="1">Medium ($11.99)</option>
-            <option value="2">Large ($13.99)</option>
+            <option value="notvalid" selected>Your Thincrust Pizza Size Options</option>
+            <option value="11.99">Medium ($11.99)</option>
+            <option value="13.99">Large ($13.99)</option>
             `;      
             break;
         case "nystyle":
             document.getElementById('size').innerHTML = `
-            <option selected>Your New York Style Pizza Size Options</option>
-            <option value="1">Medium ($11.99)</option>
-            <option value="2">Extra Large ($19.99)</option>
+            <option value="notvalid" selected>Your New York Style Pizza Size Options</option>
+            <option value="11.99">Medium ($11.99)</option>
+            <option value="19.99">Extra Large ($19.99)</option>
             `;  
             break;
         case "gluten":
             document.getElementById('size').innerHTML = `
-            <option selected>Your Gluten Pizza Size Options</option>
-            <option value="1">Medium ($11.99)</option>
+            <option value="notvalid" selected>Your Gluten Pizza Size Options</option>
+            <option value="11.99">Medium ($11.99)</option>
             `;    
             break;
     }
